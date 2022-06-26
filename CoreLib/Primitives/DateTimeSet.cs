2bfb43477b87b18157fe01a7dd2ed9cf50f8c6ba
Llewella Spencer <bassclefstudio-dev@outlook.com>
2022-06-26 01:52:01 -0400

    Improved the DateTimeSpan support for union/intersection/difference by adding separate DateTimeSet record struct.

---
+++ CoreLib/Primitives/DateTimeSet.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BassClefStudio.Core.Primitives;
+
+/// <summary>
+/// A set of <see cref="DateTimeOffset"/>s, represented by a collection of <see cref="DateTimeSpan"/>s from which the set is constructed.
+/// </summary>
+/// <param name="Spans">The collection of normalized (see <see cref="DateTimeSpanExtensions.Normalize(IEnumerable{DateTimeSpan})"/>) <see cref="DateTimeSpan"/>s which make up every interval of time which is included in this <see cref="DateTimeSet"/>.</param>
+public readonly record struct DateTimeSet(params DateTimeSpan[] Spans) : IEquatable<DateTimeSet>
+{
+    /// <summary>
+    /// Represents an empty <see cref="DateTimeSet"/> which includes no points in time.
+    /// </summary>
+    public static readonly DateTimeSet Empty = new DateTimeSet();
+
+    /// <summary>
+    /// Creates a <see cref="DateTimeSet"/> from a collection of <see cref="DateTimeSpan"/>s.
+    /// </summary>
+    /// <param name="spans">A collection of unordered <see cref="DateTimeSpan"/> spans which define the periods of time included in this set.</param>
+    /// <returns>A new <see cref="DateTimeSet"/> constructed from the normalized (see <see cref="DateTimeSpanExtensions.Normalize(IEnumerable{DateTimeSpan})"/>) <see cref="DateTimeSpan"/>s included in <paramref name="spans"/>.</returns>
+    public static DateTimeSet CreateSet(IEnumerable<DateTimeSpan> spans)
+        => new DateTimeSet(spans.Normalize().ToArray());
+
+    /// <summary>
+    /// Enumerates over <see cref="Spans"/> and returns a flat collection of all of the transition points between the starts and ends of each <see cref="DateTimeSpan"/>.
+    /// </summary>
+    /// <returns>An <see cref="IEnumerable{T}"/> of <see cref="DateTimeSpan.StartDate"/>s and <see cref="DateTimeSpan.EndDate"/>s.</returns>
+    private IEnumerable<DateTimeOffset> Points()
+    {
+        foreach(var sp in Spans)
+        {
+            yield return sp.StartDate;
+            yield return sp.EndDate;
+        }
+    }
+
+    /// <summary>
+    /// Checks if a given <see cref="DateTimeOffset"/> is present in the set.
+    /// </summary>
+    /// <param name="point">The <see cref="DateTimeOffset"/> point in time being checked.</param>
+    /// <returns>A <see cref="bool"/> indicating whether this <see cref="DateTimeSet"/> includes <paramref name="point"/>.</returns>
+    public bool Includes(DateTimeOffset point)
+        => Spans.Any(s => s.Includes(point));
+
+    /// <summary>
+    /// Calculates the inverse <see cref="DateTimeSet"/> to the current set.
+    /// </summary>
+    /// <returns>A <see cref="DateTimeSet"/> which contains all points in time which are not present in the current set. Note that the endpoints of component <see cref="DateTimeSpan"/>s are preserved in both sets.</returns>
+    public DateTimeSet Inverse()
+    {
+        DateTimeOffset? current = DateTimeOffset.MinValue;
+        List<DateTimeSpan> spans = new List<DateTimeSpan>();
+        foreach (var p in this.Points())
+        {
+            if (current == null) current = p;
+            else
+            {
+                spans.Add(new DateTimeSpan(current.Value, p)); current = null;
+            }
+        }
+        return new DateTimeSet(spans.ToArray());
+    }
+
+    /// <summary>
+    /// Calculates the union of this set with another set.
+    /// </summary>
+    /// <param name="other">The second <see cref="DateTimeSet"/> to combine.</param>
+    /// <returns>The resulting single <see cref="DateTimeSet"/> which contains all points in time contained within the current set and/or <paramref name="other"/>.</returns>
+    public DateTimeSet Union(DateTimeSet other)
+        => new DateTimeSet(this.Spans.Concat(other.Spans).Normalize().ToArray());
+
+    /// <summary>
+    /// Finds the intersection of this set with another set.
+    /// </summary>
+    /// <param name="other">The second <see cref="DateTimeSet"/> to intersect.</param>
+    /// <returns>The resulting single <see cref="DateTimeSet"/> which contains all points in time contained within both the current set and <paramref name="other"/>.</returns>
+    public DateTimeSet Intersection(DateTimeSet other)
+    {
+        int i = 0;
+        int j = 0;
+        List<DateTimeSpan> spans = new List<DateTimeSpan>();
+        while (i < this.Spans.Length && j < other.Spans.Length)
+        {
+            var a = this.Spans[i];
+            var b = other.Spans[j];
+            if (a.StartDate > b.EndDate) i++;
+            else if (b.StartDate > a.EndDate) j++;
+            else
+            {
+                spans.Add(new DateTimeSpan(
+                    (a.StartDate < b.StartDate) ? b.StartDate : a.StartDate,
+                    (a.EndDate > b.EndDate) ? b.EndDate : a.EndDate));
+            }
+        }
+        return new DateTimeSet(spans.ToArray());
+    }
+}
--- CoreLib/Primitives/DateTimeSpan.cs
+++ CoreLib/Primitives/DateTimeSpan.cs
@@ -1,4 +1,6 @@
-﻿namespace BassClefStudio.Core.Primitives
+﻿using System;
+
+namespace BassClefStudio.Core.Primitives
 {
     /// <summary>
     /// Represents a contiguous block of time between two <see cref="DateTimeOffset"/>s.
@@ -50,24 +52,41 @@
             => this.StartDate >= span.StartDate && this.EndDate <= span.EndDate;
 
         /// <summary>
-        /// Intersects this <see cref="DateTimeSpan"/> with another, returning the shared region of time (if any) between them. This is a commutative operation. Assumes <see cref="IsReversed"/> is false.
+        /// Checks if this <see cref="DateTimeSpan"/> shares any non-zero period with a given <see cref="DateTimeSpan"/>. Assumes <see cref="IsReversed"/> is false.
+        /// This operation is commutative - if <c>a</c> intersects <c>b</c>, then <c>b</c> must also intersect <c>a</c>.
         /// </summary>
-        /// <param name="other">The other <see cref="DateTimeSpan"/> being intersected.</param>
-        /// <returns>A <see cref="DateTimeSpan"/> representing the period(s) of time shared by the two <see cref="DateTimeSpan"/>s. If <c>null</c>, the two regions did not intersect at all.</returns>
-        public DateTimeSpan? Intersect(DateTimeSpan other)
+        /// <param name="span">The potentially intersecting other <see cref="DateTimeSpan"/>.</param>
+        /// <returns>A <see cref="bool"/> indicating whether <c>this</c> intersects with <paramref name="span"/>.</returns>
+        public bool Intersects(DateTimeSpan span)
+            => (this.StartDate < span.EndDate && this.EndDate > span.StartDate) || (span.StartDate < this.EndDate && span.EndDate > this.StartDate);
+
+        /// <summary>
+        /// Calculates the pair of <see cref="DateTimeSpan"/>s which represent all time except for that which is defined by this <see cref="DateTimeSpan"/>.
+        /// Note that if <see cref="StartDate"/> or <see cref="EndDate"/> are at the DateTime bounds (<see cref="DateTime.MaxValue"/> and <see cref="DateTime.MinValue"/>), the result may include zero or one elements instead.
+        /// </summary>
+        /// <returns>An array of <see cref="DateTimeSpan"/>s (with a length of usually two, but always between zero and two) covering all area except for that which is covered in <see cref="DateTimeSpan"/>). The endpoints are included in this result.</returns>
+        public DateTimeSpan[] Inverse()
         {
-            if (this.IsWithin(other)) return this;
-            else if (other.IsWithin(this)) return other;
-            else if (this.StartDate < other.StartDate && other.StartDate < this.EndDate)
-            { 
-                return new DateTimeSpan(other.StartDate, this.EndDate);
-            }
-            else if (this.StartDate < other.EndDate && other.EndDate < this.EndDate)
+            if (this.StartDate == DateTimeOffset.MinValue && this.EndDate == DateTimeOffset.MaxValue) return Array.Empty<DateTimeSpan>();
+            else if (this.StartDate == DateTimeOffset.MinValue) return new[] { new DateTimeSpan(this.EndDate, DateTimeOffset.MaxValue) };
+            else if (this.EndDate == DateTimeOffset.MaxValue) return new[] { new DateTimeSpan(DateTimeOffset.MinValue, this.StartDate) };
+            else
             {
-                return new DateTimeSpan(this.StartDate, other.EndDate);
+                return new[]
+                {
+                    new DateTimeSpan(DateTimeOffset.MinValue, this.StartDate),
+                    new DateTimeSpan(this.EndDate, DateTimeOffset.MaxValue)
+                };
             }
-            else return null;
         }
+
+        /// <summary>
+        /// Checks whether a given <see cref="DateTimeOffset"/> is included in the <see cref="DateTimeSpan"/>.
+        /// </summary>
+        /// <param name="point">The <see cref="DateTimeOffset"/> point being queried.</param>
+        /// <returns>A <see cref="bool"/> indicating whether <paramref name="point"/> is within the bounds of this <see cref="DateTimeSpan"/>.</returns>
+        public bool Includes(DateTimeOffset point)
+            => this.StartDate >= point && this.EndDate <= point;
 
         /// <inheritdoc/>
         public int CompareTo(DateTimeSpan other)
@@ -83,53 +102,30 @@
     public static class DateTimeSpanExtensions
     {
         /// <summary>
-        /// Returns the collection of <see cref="DateTimeSpan"/>s representing all contiguous periods of time during which at least one <see cref="DateTimeSpan"/> from <paramref name="events"/> and one <see cref="DateTimeSpan"/> from <paramref name="others"/> simultaneously occur. 
+        /// Normalizes a collection of <see cref="DateTimeSpan"/>s by removing all duplicate periods within the collection, as well as merging any adjacent <see cref="DateTimeSpan"/>s into a single value.
         /// </summary>
-        /// <param name="events">The first (main) set of <see cref="DateTimeSpan"/>s.</param>
-        /// <param name="others">The second set of <see cref="DateTimeSpan"/>s to intersect <paramref name="events"/> with.</param>
-        /// <returns>The resulting set of <see cref="DateTimeSpan"/>s.</returns>
-        public static IEnumerable<DateTimeSpan> Intersect(
-            this IEnumerable<DateTimeSpan> events,
-            IEnumerable<DateTimeSpan> others)
+        /// <param name="spans">The unnormalized collection of <see cref="DateTimeSpan"/>s.</param>
+        /// <returns>A collection of <see cref="DateTimeSpan"/>s equivalent to <paramref name="spans"/>, but ordered by <see cref="DateTimeSpan.StartDate"/> and without any duplicate or adjacent items. The length of the result must be equal to or less than the length of <paramref name="spans"/>.</returns>
+        public static IEnumerable<DateTimeSpan> Normalize(this IEnumerable<DateTimeSpan> spans)
         {
-            DateTimeSpan[] othersArray = others.ToArray();
-            foreach (var spanA in events)
+            DateTimeSpan? current = null;
+            foreach(var s in spans.OrderBy(k => k.StartDate))
             {
-                foreach (var spanB in othersArray)
+                if (current != null && current.Value.EndDate >= s.StartDate)
                 {
-                    DateTimeSpan? shared = spanA.Intersect(spanB);
-                    if (shared.HasValue)
+                    current = current.Value with
                     {
-                        yield return shared.Value;
-                    }
+                        EndDate = s.EndDate
+                    };
+                }
+                else
+                {
+                    yield return s;
+                    current = s;
                 }
             }
         }
-        
-        /// <summary>
-        /// Returns the collection of <see cref="DateTimeSpan"/>s representing all contiguous periods of time during which one <see cref="DateTimeSpan"/> in <paramref name="events"/> occurs and no <see cref="DateTimeSpan"/> from <paramref name="others"/> simultaneously occur. 
-        /// </summary>
-        /// <param name="events">The first (main) set of <see cref="DateTimeSpan"/>s.</param>
-        /// <param name="others">The second set of <see cref="DateTimeSpan"/>s to subtract from <paramref name="events"/>.</param>
-        /// <returns>The resulting set of <see cref="DateTimeSpan"/>s.</returns>
-        public static IEnumerable<DateTimeSpan> Difference(
-            this IEnumerable<DateTimeSpan> events,
-            IEnumerable<DateTimeSpan> others)
-        {
-            DateTimeSpan[] othersArray = others.ToArray();
-            foreach (var spanA in events)
-            {
-                foreach (var spanB in othersArray)
-                {
-                    DateTimeSpan? shared = spanA.Intersect(spanB);
-                    if (shared.HasValue)
-                    {
-                        yield return shared.Value;
-                    }
-                }
-            }
-        }
-        
+
         /// <summary>
         /// Splits this given <see cref="DateTimeSpan"/> into component <see cref="DateTimeSpan"/>s by day.
         /// </summary>
